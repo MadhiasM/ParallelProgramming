f1a05906c50239132101dd5faceae787384dd228
MadhiasM <mathias.moog1001@googlemail.com>
2024-12-20 22:54:36 +0100

    Atomic Path Method added

--- EA3/pi.c
+++ EA3/pi.c
@@ -51,13 +51,33 @@ double CalcPi_critical(int n) {
     return pi;
 }
 
+double CalcPi_atomic(int n) {
+    if (n <= 0 || n > NMAX) {
+        return 0.0;
+    }
+    double delta_x = 1.0f / n;
+    double x_i;
+    double pi = 0.0;
+
+    #pragma omp parallel for schedule(static) default(none) private(x_i) shared(n, delta_x, pi)
+    for (int i = 0; i < n; i++) {
+        x_i = i*delta_x;
+        #pragma omp atomic
+        pi += (4.0f / (1 + x_i*x_i)) * delta_x;
+    }
+    return pi;
+}
+
 float accuracy_pi(float pi_approx) {
     return (pi_approx - E_PI)/E_PI * 100;
 }
 
 int main() {
-    double pi = CalcPi_serial(N);
-    double accuracy = accuracy_pi(pi);
+    double pi;
+    double accuracy;
+
+    pi = CalcPi_serial(N);
+    accuracy = accuracy_pi(pi);
 
     printf(COLOR_BOLD "Serial:" COLOR_OFF "\n");
     printf("π ≈ %.10f (approximation)\n", pi);
@@ -74,6 +94,13 @@ int main() {
     accuracy = accuracy_pi(pi);
 
     printf(COLOR_BOLD "Critical:" COLOR_OFF "\n");
+    printf("π ≈ %.10f (approximation)\n", pi);
+    printf("Accuracy = %.10f%%\n", accuracy);
+
+    pi = CalcPi_atomic(N);
+    accuracy = accuracy_pi(pi);
+
+    printf(COLOR_BOLD "Atomic:" COLOR_OFF "\n");
     printf("π ≈ %.10f (approximation)\n", pi);
     printf("Accuracy = %.10f%%\n", accuracy);
 
